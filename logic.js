a6046ed8151c556f9c98471d31d7476c901bcff5
Lex Petrov <60630938+Alnipet@users.noreply.github.com>
2021-12-24 15:56:12 +0300

    Create logic.js

---
+++ logic.js
@@ -0,0 +1,103 @@
+let players = ['x', 'o'];
+let activePlayer = 0;
+let board;
+
+function startGame() {
+
+  //очищаем массив
+  board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ];
+
+  //рендерим игровое поле
+  renderBoard(board);
+};
+
+function click(rowNumber, columnNumber) {
+
+  //записываем в массив результат события
+  board[rowNumber][columnNumber] = players[activePlayer];
+
+  //рендерим игровое поле
+  renderBoard(board);
+
+  function rowValidation() {
+    let counterInRow = 0;
+
+    //проходим по строке поля, ищем выигрышную комбинацию
+    for (let indexInRow in board[rowNumber]) {
+      if (board[rowNumber][indexInRow] === players[activePlayer]) {
+        counterInRow++;
+        if (counterInRow === board[rowNumber].length) {
+          return true;
+        }
+      }
+    }
+  }
+
+  function columnValidation() {
+    let counterInColumn = 0;
+
+    //проходим по строке поля, ищем выигрышную комбинацию
+    for (let indexInColumn in board[columnNumber]) {
+      let counterInLeftDiag = 0;
+      if (board[indexInColumn][columnNumber] === players[activePlayer]) {
+        counterInColumn++;
+        if (counterInColumn === board.length) {
+          return true;
+        }
+      }
+    }
+  }
+
+  function leftDiagValidation() {
+    let counterInLeftDiag = 0;
+
+    //проходим по левой диагонали поля, ищем выигрышную комбинацию
+    for (let indexLeftDiag in board[rowNumber]) {
+      let counterInRightDiag = 0;
+      if (board[indexLeftDiag][indexLeftDiag] === players[activePlayer]) {
+        counterInLeftDiag++;
+        if (counterInLeftDiag === board.length) {
+          return true;
+        }
+      }
+    }
+  }
+
+  function rightDiagValidation() {
+    let counterInRightDiag = 0;
+    let rowNumberDiag = 0;
+
+    //проходим по правой диагонали поля, ищем выигрышную комбинацию
+    for (let indexRightDiag = board[rowNumber].length - 1; indexRightDiag >= 0; indexRightDiag--) {
+      if (board[rowNumberDiag][indexRightDiag] === players[activePlayer]) {
+        counterInRightDiag++;
+        rowNumberDiag++;
+        if (counterInRightDiag === board[rowNumber].length) {
+          return true;
+        }
+      }
+    }
+  }
+
+  function playerChange() {
+    if (activePlayer === 0) {
+      activePlayer = 1;
+    } else {
+      activePlayer = 0;
+    }
+    return activePlayer;
+  }
+
+  // проверяем есть ли выигрышные комбинации 
+  if (rowValidation() || columnValidation() || leftDiagValidation() || rightDiagValidation()) {
+    showWinner(activePlayer);
+  }
+  else {
+    //победных комбинаций нет, меняем игрока
+    playerChange()
+  }
+}
